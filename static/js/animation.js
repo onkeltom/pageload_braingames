cd673ad6b055dddf3db5f32c74366469a82aa449
Dominik Strohmeier <onkeltom@users.noreply.github.com>
2018-05-29 16:05:08 +0200

    Update animation.js

--- static/js/animation.js
+++ static/js/animation.js
@@ -14,16 +14,6 @@ function myFunction(args, time) {
   })
 }
 
-function endTrial(args, time) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      // here you do your stuff
-      $(args).click();
-      resolve(); // resolve the promise when it's done
-    }, time);
-  })
-}
-
 var promise = Promise.resolve();
 
 function schedule() {
@@ -38,37 +28,3 @@ $('#b1').on('click', function() {
   $('#launch').addClass('hidden');
   schedule();
 });
-
-// function myFunction(arg) {
-//     $(arg).css({ 'display': 'block'});
-// }
-//
-// function endTrial(arg) {
-//   $(arg).click();
-// }
-//
-// function schedule() {
-//     var task = tasks.shift();
-//     console.log(tasks.length)
-//     console.log(task)
-//
-//     if(task) { // && tasks.length != 0) {
-//         console.log("show function");
-//         setTimeout(function() {
-//             myFunction(task.arg);
-//             if(tasks.length) schedule();
-//         },task.delay);
-//     } //else {
-//       //console.log("end trail");
-//       //endTrial(task.arg);
-//       //if(tasks.length) schedule();
-//     //}
-// }
-//
-// $('#b1').on('click', function() {
-//   $('#launch').addClass('hidden');
-//   schedule();
-//   setTimeout( function() {
-//     $('#end-trial').click();
-//   }, 1000);
-// });
