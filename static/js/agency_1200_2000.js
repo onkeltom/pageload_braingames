dc2d40fc8f8898011d452e304fc863b6d3cbe278
Dominik Strohmeier <dominik@Dominiks-Mac-mini.local>
2018-06-19 11:00:50 +0200

    moved random number generator func (jitter) to separate file

--- static/js/agency_1200_2000.js
+++ static/js/agency_1200_2000.js
@@ -27,13 +27,13 @@ var configuration = [
                       { id: '.img6', timeout: 2000 },
                     ];
 
-// generate normal distribution scaling factor function
-function randn_bm() {
-    var u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-}
+// // generate normal distribution scaling factor function
+// function randn_bm() {
+//     var u = 0, v = 0;
+//     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+//     while(v === 0) v = Math.random();
+//     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+// }
 
 // promiseGenerator function will create a series of timeout events based on then
 // configuration variable and execute them.
@@ -50,7 +50,7 @@ function promiseGenerator({id, timeout}) {
         $(id).css({'visibility': 'visible'});
       };
       resolve();
-    }, timeout + (timeout * 0.1 * randn_bm()));
+    }, timeout + (timeout * randn_bm()));
   })
 }
 
--- static/js/agency_600_1000.js
+++ static/js/agency_600_1000.js
@@ -28,13 +28,13 @@ var configuration = [
                     ];
 
 
-// generate normal distribution scaling factor function
-function randn_bm() {
-    var u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-}
+// // generate normal distribution scaling factor function
+// function randn_bm() {
+//     var u = 0, v = 0;
+//     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+//     while(v === 0) v = Math.random();
+//     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+// }
 
 // promiseGenerator function will create a series of timeout events based on then
 // configuration variable and execute them.
@@ -51,7 +51,7 @@ function promiseGenerator({id, timeout}) {
         $(id).css({'visibility': 'visible'});
       };
       resolve();
-    }, timeout + (timeout * 0.1 * randn_bm()));
+    }, timeout + (timeout * randn_bm()));
   })
 }
 // promiseGenerator function will create a series of timeout events based on then
