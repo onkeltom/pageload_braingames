93a856ae9522eed87d9a3d382b7055dbf10c2cec
Dominik Strohmeier <dominik@Dominiks-Mac-mini.local>
2018-06-19 11:13:07 +0200

    refactoring of promiseGenerator function to separate file

--- static/js/agency_1200_5700.js
+++ static/js/agency_1200_5700.js
@@ -26,33 +26,7 @@ var configuration = [
                       { id: '.img5', timeout: 5700 },
                       { id: '.img6', timeout: 5700 },
                     ];
-//
-// // generate normal distribution scaling factor function
-// function randn_bm() {
-//     var u = 0, v = 0;
-//     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-//     while(v === 0) v = Math.random();
-//     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-// }
 
-// promiseGenerator function will create a series of timeout events based on then
-// configuration variable and execute them.
-// Using promises here makes sure that we keep the presentation order
-function promiseGenerator({id, timeout}) {
-	return new Promise((resolve, reject) => {
-  	setTimeout(() => {
-      if (jsPsych.data.getURLVariable('debug') === 'true') {
-        $('.debug').html(id + ' visible after ' + timeout + 'ms.');
-      };
-      if (id === '.test') {
-          $(id).css({'display': 'block'});
-      } else {
-        $(id).css({'visibility': 'visible'});
-      };
-      resolve();
-    }, timeout + (timeout * randn_bm()));
-  })
-}
 
 $('#b1').on('click', function() {
   $('#launch').addClass('hidden');
--- static/js/random_number.js
+++ static/js/random_number.js
@@ -4,3 +4,19 @@ function randn_bm() {
     while(v === 0) v = Math.random();
     return (Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))* 0.1;
 }
+
+function promiseGenerator({id, timeout}) {
+	return new Promise((resolve, reject) => {
+  	setTimeout(() => {
+      if (jsPsych.data.getURLVariable('debug') === 'true') {
+        $('.debug').html(id + ' visible after ' + timeout + 'ms.');
+      };
+      if (id === '.test') {
+          $(id).css({'display': 'block'});
+      } else {
+        $(id).css({'visibility': 'visible'});
+      };
+      resolve();
+    }, timeout + (timeout * randn_bm()));
+  })
+}
